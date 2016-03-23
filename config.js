8d0564b132d23f23c3b8d3f655a51bcdb503e30a
Alex Hill <alex_hill@labour.org.uk>
2016-03-23 20:09:41 +0000

    fixing config for heroku

--- config.js
+++ config.js
@@ -3,13 +3,14 @@ var fs = require('fs');
 
 var env = process.env['NODE_ENV'] || 'development';
 var json_path = path.join(__dirname, '/config', env + '.json');
+var config = {};
 
-var config = JSON.parse(fs.readFileSync(json_path));
-config.port = process.env.PORT || config.port;
-config.ip = process.env.IP || config.port;
-config.siteUrl = process.env.port || config.siteUrl;
-config.elasticHost = process.env.elasticHost || config.elasticHost;
-config.whitelist = process.env.whitelist || config.whitelist;
+if (env == "development")
+    config = JSON.parse(fs.readFileSync(json_path));
+
+else
+    config = process.env;
+
 config.env = env;
 
 module.exports = config;
