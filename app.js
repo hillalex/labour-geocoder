f9397cc7f420ede3435ac9846b7bcc73b36796c1
Alex Hill <alex_hill@labour.org.uk>
2016-03-23 20:25:05 +0000

    use heroku port env var

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var express = require('express')
     , config = require('./config');
 
 var corsOptions = {
-    origin: function(origin, callback){
+    origin: function (origin, callback) {
         var originIsWhitelisted = config.whitelist.indexOf(origin) !== -1;
         callback(null, originIsWhitelisted);
     }
@@ -15,7 +15,9 @@ app.use(require('./controllers'));
 
 server = require('http').createServer(app);
 
-var port = config.port;
-var ip = config.ip;
+var port = config.port || config.PORT;
+var ip = config.ip || config.IP;
 
 server.listen(port, ip);
+
+console.log("app listening on " + ip + ":" + port);
